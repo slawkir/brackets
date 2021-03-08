65ffb377d391bb68b10b5535e32b3273a3ac7c39
slawkir <slawkir1984@gmail.com>
2021-03-08 20:21:37 +0400

    add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let StrBr = [], PairBr = {};
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    PairBr[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  let strToArr = str.split('');
+  for(let i = 0; i < strToArr.length; i++) {
+    if(strToArr[i] === (StrBr[StrBr.length - 1] || 0)) {
+      StrBr.pop();
+    } 
+	  else {
+      	StrBr.push(PairBr[strToArr[i]]);
+	  }
+
+  }
+  if(StrBr.length == 0) {
+    return true;
+  } else {
+    return false;
+    }
+
 }
